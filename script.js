2f4aa0ab4990f05eb838d263ad92369f70008cc8
cesargarcimar <cesar.garcimar@gmail.com>
2026-02-13 13:48:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const fader = document.getElementById('fader');
     const faderValueLabel = document.getElementById('faderValue');
 
+    // Actualiza el valor del fader en tiempo real
     fader.addEventListener('input', () => {
         faderValueLabel.textContent = fader.value;
     });
