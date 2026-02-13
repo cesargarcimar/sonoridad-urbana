766e30a9295e7aec8fd510853d682bd1229a2827
cesargarcimar <cesar.garcimar@gmail.com>
2026-02-13 13:45:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,8 @@ console.log("script.js loaded");
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const startBtn = document.getElementById('startBtn');
-    const formSection = document.getElementById('formSection');
     const fader = document.getElementById('fader');
     const faderValueLabel = document.getElementById('faderValue');
-
-    startBtn.addEventListener('click', () => {
-        alert("Button works!"); // Remove later if you want
-        document.getElementById('consent').style.display = 'none';
-        formSection.style.display = 'block';
-    });
 
     fader.addEventListener('input', () => {
         faderValueLabel.textContent = fader.value;
