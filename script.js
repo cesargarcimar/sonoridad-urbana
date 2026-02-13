06d7b159b64d0e89eac33348458ba8807c4109d9
cesargarcimar <cesar.garcimar@gmail.com>
2026-02-13 13:15:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-// Comprobar que el JS se carga
 console.log("script.js cargado");
 
-// Esperar a que todo el HTML esté listo
 document.addEventListener('DOMContentLoaded', () => {
-
-    // Elementos del DOM
     const startBtn = document.getElementById('startBtn');
     const formSection = document.getElementById('formSection');
     const fader = document.getElementById('fader');
     const faderValueLabel = document.getElementById('faderValue');
 
-    // Botón de consentimiento
     startBtn.addEventListener('click', () => {
         alert("¡Botón funciona!");
         document.getElementById('consent').style.display = 'none';
         formSection.style.display = 'block';
     });
 
-    // Mostrar valor del fader en tiempo real
     fader.addEventListener('input', () => {
         faderValueLabel.textContent = fader.value;
     });
-
 });
