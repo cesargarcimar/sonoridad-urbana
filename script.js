656a8469e05f789db2f242894310d72ace214d31
cesargarcimar <cesar.garcimar@gmail.com>
2026-02-13 14:43:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-console.log("script.js loaded");
-
 document.addEventListener('DOMContentLoaded', () => {
 
+    const startBtn = document.getElementById('startBtn');
+    const consentDiv = document.getElementById('consent');
+    const formSection = document.getElementById('formSection');
     const fader = document.getElementById('fader');
     const faderValueLabel = document.getElementById('faderValue');
 
-    // Actualiza el valor del fader en tiempo real
+    // Al aceptar, mostrar segunda pantalla
+    startBtn.addEventListener('click', () => {
+        consentDiv.style.display = 'none';   // Ocultar primera pantalla
+        formSection.style.display = 'block'; // Mostrar segunda pantalla con fader y audio
+    });
+
+    // Actualizar valor del fader en tiempo real
     fader.addEventListener('input', () => {
         faderValueLabel.textContent = fader.value;
     });
